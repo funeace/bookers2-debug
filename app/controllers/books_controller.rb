c8317b8c7d50d9c35b3139c782f417aa0fc15529
Kengo <funeace@gmail.com>
2020-03-01 15:32:38 +0900

    [Finish]Advanced_2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,34 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
 
   def show
   	@book = Book.find(params[:id])
+    @user_new = @book.user
+    @book_new = Book.new
   end
 
   def index
+    @book = Book.new()
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  	book = current_user.books.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+  	if book.save #入力されたデータをdbに保存する。
+  		redirect_to book_path(book), notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
+      @book = book
+      @user = User.find(current_user.id)
   		@books = Book.all
   		render 'index'
   	end
   end
 
-  def edit
+  def edit    
   	@book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+      redirect_to books_path   
+    end
   end
 
 
@@ -27,22 +36,22 @@ class BooksController < ApplicationController
   def update
   	@book = Book.find(params[:id])
   	if @book.update(book_params)
-  		redirect_to @book, notice: "successfully updated book!"
+  		redirect_to book_path(@book), notice: "successfully updated book!"
   	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
   		render "edit"
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title,:body)
   end
 
 end
